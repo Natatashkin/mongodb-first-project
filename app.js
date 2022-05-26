6f19353a26e90089846823a2ae205e0d3aa56901
Natatashkin <cs070283sng@gmail.com>
2022-05-26 21:57:38 +0300

    fix app

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-//natatashkin, 69MjDN8iWkw1NdBJ
-
 const mongoose = require("mongoose");
+const dotenv = require("dovenv");
 
-const DB_HOST =
-  "mongodb+srv://natatashkin:69MjDN8iWkw1NdBJ@cluster0.iy8sf.mongodb.net/shop?retryWrites=true&w=majority";
+dotenv.config();
+
+const { DB_HOST } = process.env;
 
 mongoose
   .connect(DB_HOST)
   .then(() => console.log("DB connect"))
-  .catch((error) => console.log(error.message));
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
